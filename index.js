43a058c43fbe78e020ddcd9ab27d2c5feb76abb7
Birendra Singh <birendra@DESKTOP-LPV9HSD.localdomain>
2026-03-18 06:18:47 +0000

    fix start script and port

--- index.js
+++ index.js
@@ -80,5 +80,5 @@ async function sendMessage(to, text) {
   );
 }
 
-app.listen(3000, () => console.log('Bot running on port 3000'));
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Bot running on port ${PORT}`));
